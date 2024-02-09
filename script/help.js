67c09a121ee03973532b78292395e70ec54a5d93
AndrieDiosk <155274915+AndrieDiosk@users.noreply.github.com>
2024-02-09 19:05:46 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -82,14 +82,14 @@ module.exports.run = async function({
           cooldown,
           hasPrefix
         } = command;
-        const roleMessage = role !== undefined ? (role === 0 ? 'Permission: user' : (role === 1 ? 'Permission: admin' : (role === 2 ? 'Permission: thread Admin' : (role === 3 ? 'Permission: super Admin' : '')))) : '';
-        const aliasesMessage = aliases.length ? `Aliases: ${aliases.join(', ')}\n` : '';
-        const descriptionMessage = description ? `Description: ${description}\n` : '';
-        const usageMessage = usage ? `Usage: ${usage}\n` : '';
-        const creditsMessage = credits ? `Credits: ${credits}\n` : '';
-        const versionMessage = version ? `Version: ${version}\n` : '';
-        const cooldownMessage = cooldown ? `Cooldown: ${cooldown} second(s)\n` : '';
-        const message = ` 「 Command 」\n\nName: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
+        const roleMessage = role !== undefined ? (role === 0 ? '❯ permission: user' : (role === 1 ? '❯ permission: admin' : (role === 2 ? '❯ permission: thread Admin' : (role === 3 ? '❯ permission: super Admin' : '')))) : '';
+        const aliasesMessage = aliases.length ? `❯ aliases: ${aliases.join(', ')}\n` : '';
+        const descriptionMessage = description ? `${description}\n` : '';
+        const usageMessage = usage ? `❯ usage: ${usage}\n` : '';
+        const creditsMessage = credits ? `❯ credits: ${credits}\n` : '';
+        const versionMessage = version ? `❯ version: ${version}\n` : '';
+        const cooldownMessage = cooldown ? `❯ cooldown: ${cooldown} second(s)\n` : '';
+        const message = ` 「 ${name} 」\n${descriptionMessage}\n${versionMessage}${roleMessage}\n${aliasesMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
         api.sendMessage(message, event.threadID, event.messageID);
       } else {
         api.sendMessage('Command not found.', event.threadID, event.messageID);
